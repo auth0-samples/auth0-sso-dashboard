26cd23c9f3634e33e1ebacc3f5e2138833aba8af
Nathan Totten <ntotten@auth0.com>
2015-02-19 16:43:18 -0800

    Api proxy, auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var jwt = require('express-jwt');
+var proxy = require('json-proxy');
 
 var dotenv = require('dotenv');
 dotenv.load();
@@ -28,17 +29,44 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    auth0_client_id: process.env.AUTH0_CLIENT_ID,
+    auth0_domain: process.env.AUTH0_DOMAIN
+  });
+});
+
 // All requests to the API require authentication
 app.use('/api', authenticate);
-
-app.use('/', require('./routes/index'));
-app.use('/api', require('./routes/api'));
+app.use('/api', function(req, res, next) {
+  if (req.user && req.user.roles && req.user.roles.indexOf('admin') > -1) {
+    next();
+  }
+  throw { status: 403, code: 'unathorized_api_call', message: 'Unauthorized to access this resource.'};
+});
+/*
+app.use('/api', function(req, res, next) {
+  // This is where we could check allowed actions
+  next();
+});
+*/
+app.use(proxy.initialize({
+  proxy: {
+    forward: {
+      '/api/(.*)': 'https://' + process.env.AUTH0_DOMAIN + '/api/v2/$1'
+    },
+    headers: {
+      'Authorization': 'Bearer ' + process.env.AUTH0_API_TOKEN
+    }
+  }
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+var err = new Error('Not Found');
+err.status = 404;
+next(err);
 });
 
 // error handlers
@@ -46,24 +74,33 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.json({
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    var data = {
+      message: err.message,
+      error: err
+    };
+    handleError(data, req, res);
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  var data = {
+    message: err.message,
+    error: {}
+  };
+  handleError(data, req, res);
 });
 
+var handleError = function(err, req, res) {
+  if (req.path.indexOf('/api') == 0) {
+    res.json(err);
+  } else {
+    res.render('error', err);
+  }
+}
 
 module.exports = app;
--- routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/api/ping', function(req, res) {
-  res.send(200, {text: "All good. You only get this message if you're authenticated"});
-})
-
-module.exports = router;
