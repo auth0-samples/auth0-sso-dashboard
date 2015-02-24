97c090862a9b8e2c53bd20249754dc1f9c2ba97f
Nathan Totten <ntotten@auth0.com>
2015-02-24 12:40:48 -0800

    Support for custom logos

--- app/lib/components/ApplicationListItem.react.js
+++ app/lib/components/ApplicationListItem.react.js
@@ -4,16 +4,11 @@ var React = require('react');
 var ApplicationItem = React.createClass({
   render: function() {
     var app = this.props.app;
-    var logoUrl = '/img/logos/auth0.png';
-    var loginUrl = this.props.app.login_url;
-    if (app.logo) {
-      logoUrl = app.logo;
-    }
 
     return (
       <div className="app">
-        <a href={loginUrl}><img src={logoUrl} /></a>
-        <a className="name" href={loginUrl}>{app.name}</a>
+        <a href={app.login_url}><img src={app.logo_url} /></a>
+        <a className="name" href={app.login_url}>{app.name}</a>
       </div>
     );
   }
