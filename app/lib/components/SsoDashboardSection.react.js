6ef19a0a4e8c1f51f3558df9f2d7a1e6248572b4
Nathan Totten <ntotten@auth0.com>
2015-03-25 15:55:48 -0700

    Added simple app search

--- app/lib/components/SsoDashboardSection.react.js
+++ app/lib/components/SsoDashboardSection.react.js
@@ -4,20 +4,12 @@ var ApplicationListItem = require('./ApplicationListItem.react');
 var UI = require('./UI.react');
 var Mixins = require('../mixins');
 var AppActions = require('../actions/AppActions');
+var _ = require('lodash');
 
 function getStateFromStores() {
   return {
     apps: UserAppStore.get()
   };
-}
-
-function getAppListItem(app) {
-  return (
-    <ApplicationListItem
-      key={app.client_id}
-      app={app}
-    />
-  );
 }
 
 var ApplicationList = React.createClass({
@@ -38,15 +30,41 @@ var ApplicationList = React.createClass({
     UserAppStore.removeChangeListener(this._onChange);
   },
 
+  onSearchChange: function(event) {
+    this.setState({ searchTerm: event.target.value });
+  },
+
   render: function() {
     var message = "It looks like you haven't been authorized to use any applications yet.";
     var inner = (<h3>{message}</h3>);
     if (this.state.apps && this.state.apps.length > 0) {
-      inner = this.state.apps.map(getAppListItem);
+      inner = this.state.apps.map(function(app) {
+        if (this.state.searchTerm) {
+          var tokens = app.name.toLowerCase().split(' ');
+          var q = this.state.searchTerm.toLowerCase();
+          var include = false;
+          tokens.map(function(token) {
+            include = include || _.startsWith(token, q);
+          });
+          if (!include) {
+            return;
+          }
+        }
+
+        return (
+          <ApplicationListItem key={app.client_id} app={app} />
+        );
+      }, this);
     }
+
     return (
       <div className="container">
-        <UI.PageHeader title="Your Applications" />
+        <UI.PageHeader title="Your Applications">
+          <div className="search-box">
+            <span className="glyphicon glyphicon-search"></span>
+            <input type="text" placeholder="Search" onChange={this.onSearchChange} />
+          </div>
+        </UI.PageHeader>
         <div className="row" id="apps">
           {inner}
         </div>
